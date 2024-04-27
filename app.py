a20f3f094a5a2d55ad44ce4dcf84fced9368469f
Ahmad Shahzad <a.shahzad2000july@gmail.com>
2024-04-27 16:49:04 -0700

    changes

---
+++ app.py
@@ -0,0 +1,508 @@
+
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_community.document_loaders import WebBaseLoader
+from langchain_community.vectorstores import Chroma
+from langchain_community.embeddings import GPT4AllEmbeddings
+from langchain_openai import OpenAIEmbeddings
+
+import os
+from langchain.prompts import PromptTemplate
+from langchain_core.output_parsers import JsonOutputParser
+from langchain_community.chat_models import ChatOpenAI
+
+from langchain.prompts import PromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+
+from langchain.prompts import PromptTemplate
+from langchain_core.output_parsers import JsonOutputParser
+from langchain_community.tools.tavily_search import TavilySearchResults
+from typing_extensions import TypedDict
+from typing import List
+import requests
+from langchain.schema import Document
+from langgraph.graph import END, StateGraph
+from flask import Flask, render_template, request, redirect
+import time
+
+
+
+
+
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+load_dotenv()
+
+# Now you can access your API keys securely
+openai_api_key = os.getenv('OPENAI_API_KEY')
+tavily_api_key = os.getenv('TAVILY_API_KEY')
+
+
+
+web_search_tool = TavilySearchResults(k=3)
+
+
+
+### Index
+urls = [
+    "https://lilianweng.github.io/posts/2023-06-23-agent/",
+    "https://lilianweng.github.io/posts/2023-03-15-prompt-engineering/",
+    "https://lilianweng.github.io/posts/2023-10-25-adv-attack-llm/",
+]
+
+docs = [WebBaseLoader(url).load() for url in urls]
+docs_list = [item for sublist in docs for item in sublist]
+
+text_splitter = RecursiveCharacterTextSplitter.from_tiktoken_encoder(
+    chunk_size=250, chunk_overlap=0
+)
+doc_splits = text_splitter.split_documents(docs_list)
+
+# Add to vectorDB
+vectorstore = Chroma.from_documents(
+    documents=doc_splits,
+    collection_name="rag-chroma",
+    embedding=OpenAIEmbeddings(),
+)
+retriever = vectorstore.as_retriever()
+
+
+### Retrieval Grader 
+
+llm = ChatOpenAI(model_name="gpt-4", temperature=0.3)
+
+
+
+prompt = PromptTemplate(
+    template="""<|begin_of_text|><|start_header_id|>system<|end_header_id|> You are a grader assessing relevance 
+    of a retrieved document to a user question. If the document contains keywords related to the user question, 
+    grade it as relevant. It does not need to be a stringent test. The goal is to filter out erroneous retrievals. \n
+    Give a binary score 'yes' or 'no' score to indicate whether the document is relevant to the question. \n
+    Provide the binary score as a JSON with a single key 'score' and no premable or explaination.
+     <|eot_id|><|start_header_id|>user<|end_header_id|>
+    Here is the retrieved document: \n\n {document} \n\n
+    Here is the user question: {question} \n <|eot_id|><|start_header_id|>assistant<|end_header_id|>
+    """,
+    input_variables=["question", "document"],
+)
+
+retrieval_grader = prompt | llm | JsonOutputParser()
+
+
+
+### Generate
+
+
+# Prompt
+prompt = PromptTemplate(
+    template="""<|begin_of_text|><|start_header_id|>system<|end_header_id|> You are an assistant for question-answering tasks. 
+    Use the following pieces of retrieved context to answer the question. If you don't know the answer, just say that you don't know. 
+    Use three sentences maximum and keep the answer concise <|eot_id|><|start_header_id|>user<|end_header_id|>
+    Question: {question} 
+    Context: {context} 
+    Answer: <|eot_id|><|start_header_id|>assistant<|end_header_id|>""",
+    input_variables=["question", "document"],
+)
+
+
+# Post-processing
+def format_docs(docs):
+    return "\n\n".join(doc.page_content for doc in docs)
+
+# Chain
+rag_chain = prompt | llm | StrOutputParser()
+
+ 
+
+
+# Prompt
+prompt = PromptTemplate(
+    template=""" <|begin_of_text|><|start_header_id|>system<|end_header_id|> You are a grader assessing whether 
+    an answer is grounded in / supported by a set of facts. Give a binary score 'yes' or 'no' score to indicate 
+    whether the answer is grounded in / supported by a set of facts. Provide the binary score as a JSON with a 
+    single key 'score' and no preamble or explanation. <|eot_id|><|start_header_id|>user<|end_header_id|>
+    Here are the facts:
+    \n ------- \n
+    {documents} 
+    \n ------- \n
+    Here is the answer: {generation}  <|eot_id|><|start_header_id|>assistant<|end_header_id|>""",
+    input_variables=["generation", "documents"],
+)
+
+hallucination_grader = prompt | llm | JsonOutputParser()
+
+
+
+# Prompt
+prompt = PromptTemplate(
+    template="""<|begin_of_text|><|start_header_id|>system<|end_header_id|> You are a grader assessing whether an 
+    answer is useful to resolve a question. Give a binary score 'yes' or 'no' to indicate whether the answer is 
+    useful to resolve a question. Provide the binary score as a JSON with a single key 'score' and no preamble or explanation.
+     <|eot_id|><|start_header_id|>user<|end_header_id|> Here is the answer:
+    \n ------- \n
+    {generation} 
+    \n ------- \n
+    Here is the question: {question} <|eot_id|><|start_header_id|>assistant<|end_header_id|>""",
+    input_variables=["generation", "question"],
+)
+
+answer_grader = prompt | llm | JsonOutputParser()
+
+
+prompt = PromptTemplate(
+    template="""<|begin_of_text|><|start_header_id|>system<|end_header_id|> You are an expert at routing a 
+    user question to a vectorstore or web search. Use the vectorstore for questions on LLM  agents, 
+    prompt engineering, and adversarial attacks. You do not need to be stringent with the keywords 
+    in the question related to these topics. Otherwise, use web-search. Give a binary choice 'web_search' 
+    or 'vectorstore' based on the question. Return the a JSON with a single key 'datasource' and 
+    no premable or explaination. Question to route: {question} <|eot_id|><|start_header_id|>assistant<|end_header_id|>""",
+    input_variables=["question"],
+)
+
+question_router = prompt | llm | JsonOutputParser()
+
+
+class GraphState(TypedDict):
+    """
+    Represents the state of our graph.
+
+    Attributes:
+        question: question
+        generation: LLM generation
+        web_search: whether to add search
+        documents: list of documents 
+    """
+    question : str
+    generation : str
+    web_search : str
+    documents : List[str]
+
+
+### Nodes
+
+def retrieve(state):
+    """
+    Retrieve documents from vectorstore
+
+    Args:
+        state (dict): The current graph state
+
+    Returns:
+        state (dict): New key added to state, documents, that contains retrieved documents
+    """
+    print("---RETRIEVE---")
+    question = state["question"]
+
+    # Retrieval
+    documents = retriever.invoke(question)
+    return {"documents": documents, "question": question}
+
+def generate(state):
+    """
+    Generate answer using RAG on retrieved documents
+
+    Args:
+        state (dict): The current graph state
+
+    Returns:
+        state (dict): New key added to state, generation, that contains LLM generation
+    """
+    print("---GENERATE---")
+    question = state["question"]
+    documents = state["documents"]
+    
+    # RAG generation
+    generation = rag_chain.invoke({"context": documents, "question": question})
+    return {"documents": documents, "question": question, "generation": generation}
+
+def grade_documents(state):
+    """
+    Determines whether the retrieved documents are relevant to the question
+    If any document is not relevant, we will set a flag to run web search
+
+    Args:
+        state (dict): The current graph state
+
+    Returns:
+        state (dict): Filtered out irrelevant documents and updated web_search state
+    """
+
+    print("---CHECK DOCUMENT RELEVANCE TO QUESTION---")
+    question = state["question"]
+    documents = state["documents"]
+    
+    # Score each doc
+    filtered_docs = []
+    web_search = "No"
+    for d in documents:
+        score = retrieval_grader.invoke({"question": question, "document": d.page_content})
+        grade = score['score']
+        # Document relevant
+        if grade.lower() == "yes":
+            print("---GRADE: DOCUMENT RELEVANT---")
+            filtered_docs.append(d)
+        # Document not relevant
+        else:
+            print("---GRADE: DOCUMENT NOT RELEVANT---")
+            # We do not include the document in filtered_docs
+            # We set a flag to indicate that we want to run web search
+            web_search = "Yes"
+            continue
+    return {"documents": filtered_docs, "question": question, "web_search": web_search}
+    
+def web_search(state):
+    """
+    Web search based based on the question
+
+    Args:
+        state (dict): The current graph state
+
+    Returns:
+        state (dict): Appended web results to documents
+    """
+
+    print("---WEB SEARCH---")
+    question = state["question"]
+    documents = state["documents"]
+
+    # Web search
+    docs = web_search_tool.invoke({"query": question})
+    web_results = "\n".join([d["content"] for d in docs])
+    web_results = Document(page_content=web_results)
+    if documents is not None:
+        documents.append(web_results)
+    else:
+        documents = [web_results]
+    return {"documents": documents, "question": question}
+
+### Conditional edge
+
+def route_question(state):
+    """
+    Route question to web search or RAG.
+
+    Args:
+        state (dict): The current graph state
+
+    Returns:
+        str: Next node to call
+    """
+
+    print("---ROUTE QUESTION---")
+    question = state["question"]
+    print(question)
+    source = question_router.invoke({"question": question})  
+    print(source)
+    print(source['datasource'])
+    if source['datasource'] == 'web_search':
+        print("---ROUTE QUESTION TO WEB SEARCH---")
+        return "websearch"
+    elif source['datasource'] == 'vectorstore':
+        print("---ROUTE QUESTION TO RAG---")
+        return "vectorstore"
+
+def decide_to_generate(state):
+    """
+    Determines whether to generate an answer, or add web search
+
+    Args:
+        state (dict): The current graph state
+
+    Returns:
+        str: Binary decision for next node to call
+    """
+
+    print("---ASSESS GRADED DOCUMENTS---")
+    question = state["question"]
+    web_search = state["web_search"]
+    filtered_documents = state["documents"]
+
+    if web_search == "Yes":
+        # All documents have been filtered check_relevance
+        # We will re-generate a new query
+        print("---DECISION: ALL DOCUMENTS ARE NOT RELEVANT TO QUESTION, INCLUDE WEB SEARCH---")
+        return "websearch"
+    else:
+        # We have relevant documents, so generate answer
+        print("---DECISION: GENERATE---")
+        return "generate"
+
+
+def push_to_fakeJSON(state):
+
+    print("---Push code to fakeJSON---")
+
+    generation = state["generation"]
+
+    url = 'https://jsonplaceholder.typicode.com/posts'
+
+    # Data to be sent in the POST request
+    data = {
+        "title": "Chat Posted",
+        "body": generation,
+        "userId": 1
+    }
+
+    # Send a POST request
+    response = requests.post(url, json=data)
+
+    return
+
+
+### Conditional edge
+
+def grade_generation_v_documents_and_question(state):
+    """
+    Determines whether the generation is grounded in the document and answers question.
+
+    Args:
+        state (dict): The current graph state
+
+    Returns:
+        str: Decision for next node to call
+    """
+
+    print("---CHECK HALLUCINATIONS---")
+    question = state["question"]
+    documents = state["documents"]
+    generation = state["generation"]
+
+    score = hallucination_grader.invoke({"documents": documents, "generation": generation})
+    grade = score['score']
+
+    # Check hallucination
+    if grade == "yes":
+        print("---DECISION: GENERATION IS GROUNDED IN DOCUMENTS---")
+        # Check question-answering
+        print("---GRADE GENERATION vs QUESTION---")
+        score = answer_grader.invoke({"question": question,"generation": generation})
+        grade = score['score']
+        if grade == "yes":
+            print("---DECISION: GENERATION ADDRESSES QUESTION---")
+            return "useful"
+        else:
+            print("---DECISION: GENERATION DOES NOT ADDRESS QUESTION---")
+            return "not useful"
+    else:
+        print("---DECISION: GENERATION IS NOT GROUNDED IN DOCUMENTS, RE-TRY---")
+        return "not supported"
+
+
+workflow = StateGraph(GraphState)
+
+# Define the nodes
+workflow.add_node("websearch", web_search) # web search
+workflow.add_node("retrieve", retrieve) # retrieve
+workflow.add_node("grade_documents", grade_documents) # grade documents
+workflow.add_node("generate", generate) # generatae
+workflow.add_node("push_to_fakeJSON",push_to_fakeJSON) # push to fake json
+
+
+# Build graph
+workflow.set_conditional_entry_point(
+    route_question,
+    {
+        "websearch": "websearch",
+        "vectorstore": "retrieve",
+    },
+)
+
+workflow.add_edge("retrieve", "grade_documents")
+workflow.add_conditional_edges(
+    "grade_documents",
+    decide_to_generate,
+    {
+        "websearch": "websearch",
+        "generate": "generate",
+    },
+)
+workflow.add_edge("websearch", "generate")
+workflow.add_conditional_edges(
+    "generate",
+    grade_generation_v_documents_and_question,
+    {
+        "not supported": "generate",
+        "useful": 'push_to_fakeJSON',
+        "not useful": "websearch",
+    },
+)
+
+workflow.add_edge("push_to_fakeJSON", END)
+
+lang_graph = workflow.compile()
+
+cwd = os.getcwd()
+
+history_file = os.path.join(cwd, f'chat_history.txt')
+
+# Create a new chat history file
+with open(history_file, 'w') as f:
+    f.write('\n')
+
+# Initialize chat history
+chat_history = ''
+
+# Create a Flask web application
+app = Flask(__name__)
+
+
+
+
+names = 'BOT'
+
+# Function to handle user chat input
+def chat(user_input):
+
+    inputs = {"question": user_input}
+
+
+    
+    out=lang_graph.invoke(inputs)
+
+
+    global chat_history
+    global names
+    global chatgpt_output
+
+
+    current_day = time.strftime("%d/%m", time.localtime())
+    current_time = time.strftime("%H:%M:%S", time.localtime())
+    chat_history += f'\nUser: {user_input}\n'
+
+
+    # chatgpt_raw_output = chatcompletion(user_input, impersonated_role, explicit_input, chat_history).replace(f'{name}:', '')
+    chatgpt_output = f"{names}: {out['generation']}"
+    chat_history += chatgpt_output + '\n'
+    with open(history_file, 'a') as f:
+        f.write('\n'+ current_day+ ' '+ current_time+ ' User: ' +user_input +' \n' + current_day+ ' ' + current_time+  ' ' +  chatgpt_output + '\n')
+        f.close()
+    return out['generation']
+
+# Function to get a response from the chatbot
+def get_response(userText):
+    return chat(userText)
+
+# Define app routes
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/get")
+# Function for the bot response
+def get_bot_response():
+    userText = request.args.get('msg')
+    return str(get_response(userText))
+
+@app.route('/refresh')
+def refresh():
+    time.sleep(600) # Wait for 10 minutes
+    return redirect('/refresh')
+
+# Run the Flask app
+if __name__ == "__main__":
+    app.run(debug=True)
+
+
+
+
+
